7f5e7bec66434a026bb0861f1ab8670534eb6b1b
Pawel Socha <merlin@rockowy.net>
2018-02-24 18:54:43 +0100

    Add router package

---
+++ router/router.go
@@ -0,0 +1,31 @@
+package router
+
+import "github.com/pawelsocha/kryptond/database"
+
+// Router describe basic info about router
+type Router struct {
+	Name           string `gorm:"column:name"`
+	PrivateAddress string `gorm:"column:priv"`
+	PublicAddress  string `gorm:"column:public"`
+}
+
+// GetRoutersList get from database list of a router to configure.
+// All router must be Konfiguracja/Configuration type
+func GetRoutersList(db *database.SQLStorage) ([]Router, error) {
+	if db.Error != nil {
+		return nil, db.Error
+	}
+
+	query := `SELECT n.name, 
+                     INET_NTOA(n.ipaddr) priv, 
+                     INET_NTOA(n.ipaddr_pub) public
+                FROM lms.netdevices nd 
+           LEFT JOIN nastypes t on (t.id=nd.nastype) 
+           LEFT JOIN vnodes n ON (n.netdev=nd.netnodeid) 
+               WHERE t.name='Konfiguracja'
+                 AND n.ownerid=0`
+
+	var ret []Router
+	err := db.Connection.Raw(query).Find(&ret).Error
+	return ret, err
+}
