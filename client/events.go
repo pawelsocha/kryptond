9380d7fd983327efb5f3426aaaab7794ae6154c4
Pawel Socha <merlin@rockowy.net>
2018-02-25 19:51:07 +0100

    Client: add event

---
+++ client/events.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/pawelsocha/kryptond/config"
+	"github.com/pawelsocha/kryptond/database"
+	. "github.com/pawelsocha/kryptond/logging"
+)
+
+type Event struct {
+	ID         int64     `gorm:"column:id"`
+	CustomerId int64     `gorm:"column:customerid"`
+	NodeId     int64     `gorm:"column:nodeid"`
+	Status     string    `gorm:"column:status"`
+	Start      time.Time `gorm:"column:start"`
+	Finish     time.Time `gorm:"column:finish"`
+	Operation  string    `gorm:"column:type"`
+	Table      string    `gorm:"column:source"`
+}
+
+func (e Event) TableName() string {
+	return "kryptond_events"
+}
+
+type Events []Event
+
+func Subscribe(ctx context.Context, cfg *config.Config) {
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(time.Second * 60):
+				e, err := CheckEvents(cfg)
+				Log.Infof("event: %v, err: %s", e, err)
+				return
+			}
+		}
+	}()
+}
+
+func CheckEvents(cfg *config.Config) (Events, error) {
+	db, err := database.Database(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Connection.Close()
+	var events Events
+	if err = db.Connection.Where("status = ?", "NEW").Find(&events).Error; err != nil {
+		return nil, err
+	}
+
+	err = db.Connection.Table("kryptond_events").Where("status = ?", "NEW").Updates(map[string]interface{}{"status": "RUNNING"}).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
